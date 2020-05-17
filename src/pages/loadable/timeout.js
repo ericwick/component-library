a4adf2cd498de49c808f8dab3fd513cb21368dd0
wick <wickham.eaw@gmail.com>
2020-05-16 20:09:19 -0400

    clean up

--- src/pages/loadable/timeout.js
+++ src/pages/loadable/timeout.js
@@ -1,18 +1,17 @@
-import React from "react";
-// Styles
-import "./styles/container.scss";
-import "./styles/text.scss";
-import "./styles/button.scss";
+import React from 'react';
+import './styles/container.scss';
+import './styles/text.scss';
+import './styles/button.scss';
 
 const Timeout = (props) => {
-    return (
-        <div className="container">
-            <p className="static-text">Sum'n went wrong bruh...</p>
-            <button className="loadable-button" onClick={props.retry}>
-                Retry
-            </button>
-        </div>
-    );
+  return (
+    <div className="container">
+      <p className="static-text">Sum'n went wrong bruh...</p>
+      <button className="loadable-button" onClick={props.retry}>
+        Retry
+      </button>
+    </div>
+  );
 };
 
 export default Timeout;
