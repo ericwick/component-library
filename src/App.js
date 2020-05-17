a160a1feeafcaafb275232717b6b6fb901ad1ac7
wick <wickham.eaw@gmail.com>
2020-05-16 20:02:23 -0400

    linting, unit tests set up

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-import React from "react";
-// import Main from
-import SideNav from "./components/sidenav/sidenav";
-import route from "./pages/loadable/routes";
+import React from 'react';
+import SideNav from './components/sidenav/sidenav';
+import route from './pages/loadable/routes';
 
 class App extends React.Component {
-    render() {
-        return (
-            <div className="main">
-                <SideNav />
-                {route}
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="main">
+        <SideNav />
+        {route}
+      </div>
+    );
+  }
 }
 
 export default App;
